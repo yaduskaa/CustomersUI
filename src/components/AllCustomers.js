9f9fc5d90136ae2d54e2cb7e609fdea987f68a38
yaduskaa <92705712+yaduskaa@users.noreply.github.com>
2021-10-27 14:31:38 +0530

    Update AllCustomers.js

--- src/components/AllCustomers.js
+++ src/components/AllCustomers.js
@@ -1,22 +1,28 @@
 import Customer from './Customer'
-import {Card,CardTitle,CardBody } from 'reactstrap'
+import {Button, Card,CardBody,CardText,CardTitle,Container} from 'reactstrap'
 import React,{ useEffect,useState } from 'react'
 import axios from 'axios'
 import base_url from '../service/serviceapi'
+import {Link} from 'react-router-dom'
 import {toast} from 'react-toastify'
-const AllCustomers=()=>{
-    const [customers,setCustomers]=useState([])
+import UpdateCustomers from './UpdateCustomers'
+ const AllCustomers=()=>{
+ const [customers,setCustomers]=useState([])
     useEffect(()=>{
     getAllCustomersFromServer()},[])
+    //console.log(customers);
 
+    const UpdateCustomers=(id)=>{
+      setCustomers(customers.filter((customer)=>customer.id!==id))
+    }
     const getAllCustomersFromServer=()=>{
        // let that=this
-      axios.get(`${base_url}/`).then(
-      (response)=>{
-          setCustomers(response.data)
-          console.log(customers)
-          console.log(response.data)
-        },
+      axios.get(`${base_url}/get/customers`).then(
+      (response)=>{ 
+           setCustomers(response.data)
+          //console.log(customers)
+          //console.log(response)
+        }
           //toast("All Customers are loaded")},
            
            )
@@ -26,16 +32,20 @@ const AllCustomers=()=>{
     return (
         
         <Card body inverse color="primary">
-            <CardTitle className="display-2">
+            <CardTitle className="display-3">
                 All Customers
                 </CardTitle>
                
                 {
+                    //customers=response.data
                     customers.length>0?
-                       customers.map((customer) => {
-                       <Customer key={customer.id} customer={customer}/>
+                    customers.map((customer)=>(
+                        <Customer key={customer.id} customer={customer} update={UpdateCustomers}/>
+                    ))
+                       
                         
-                   } )
+                  
+                 //console.log(customers)
                    
     :
     "No Customers are available"
