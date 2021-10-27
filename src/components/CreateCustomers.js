d9c84cc118fde520dec93915093e58b8eb0cd0fc
yaduskaa <92705712+yaduskaa@users.noreply.github.com>
2021-10-27 14:34:34 +0530

    Update CreateCustomers.js

--- src/components/CreateCustomers.js
+++ src/components/CreateCustomers.js
@@ -1,17 +1,41 @@
+import axios from 'axios'
+import { useState } from 'react'
 import {Card, CardBody, CardTitle,Input,Container,Button} from 'reactstrap'
+import base_url from '../service/serviceapi'
 const CreateCustomers=()=>{
+    const[customer,setCustomers]=useState({})
+    const handleForm= (param)=>{
+        postData(customer)
+        param.preventDefault()
+    }
+    const postData=(data)=>{
+        axios.post(`${base_url}/customers`,data).then(
+            (response)=>{
+                console.log("created successfully")
+            }
+        )
+    }
     return(
         <Card body inverse color="primary">
             <CardBody>
-                <form onSubmit="#">
+                <form onSubmit={handleForm}>
                     <CardTitle className="display-2">
                         New Customer
                         </CardTitle>
-                        <Input placeholder="Enter the name" onChange="#"/>
+                        <Input placeholder="Enter the name" 
+                        onChange={(param)=>{
+                            setCustomers({...customer,name:param.target.value})
+                        }}/>
                         <br/>
-                        <Input placeholder="Enter the phoneno here" onChange="#"/>
+                        <Input placeholder="Enter the phoneno here" 
+                        onChange={(param)=>{
+                            setCustomers({...customer,phoneno:param.target.value})
+                        }}/>
                         <br/>
-                        <Input placeholder="Enter the emailid here" onChange="#"/>
+                        <Input placeholder="Enter the emailid here" 
+                        onChange={(param)=>{
+                            setCustomers({...customer,emailid:param.target.value})
+                        }}/>
                         <br/>
                         <Container>
                             <Button type="submit"color="success" style={{marginRight:15+"px"}}>
